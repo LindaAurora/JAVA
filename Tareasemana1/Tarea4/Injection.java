e2e9cf781c9c6983586000344cdefe45dec10148
lindaaurora <112457076+LindaAurora@users.noreply.github.com>
2023-02-24 13:18:22 -0600

    Documentando

--- Tareasemana1/Tarea4/Injection.java
+++ Tareasemana1/Tarea4/Injection.java
@@ -10,24 +10,24 @@ public class Injection {
         dependent.doSomething();
     }
 }
-
+///La clase dependent es un objeto que tiene una dependencia de la clase dependency
 class Dependent {
     private Dependency dependency;
 
     public Dependent(Dependency dependency) {
         this.dependency = dependency;
     }
-
+//Aqui se llama a los metodos SET y GET
     public void doSomething() {
         dependency.setA(2);
         System.out.println(dependency.getA());
         dependency.doSomethingElse();
     }
 }
-
+//Aqui la clase dependency tiene una variable de instancia llamada a
 class Dependency {
     int a;
-
+//La instancia se puede obtener y llamar a traves de getA y setA
     public int getA() {
         return a;
     }
@@ -35,7 +35,7 @@ class Dependency {
     public void setA(int a) {
         this.a = a;
     }
-
+//aqui se imprime un mensaje que incluye el valor de a
     public void doSomethingElse() {
         System.out.println("El número es " + a);
     }
